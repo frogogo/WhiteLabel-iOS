d6a544176f4da39bf0405563d8f5a5464afa62e5
Oleg Mosyagin <o.lego.work@gmail.com>
2021-04-21 21:19:40 +0500

    Add phone field handling

--- WhiteLabel-iOS/Common/Screens and navigation/Auth/Phone enter screen/PhoneEnterScreenController.swift
+++ WhiteLabel-iOS/Common/Screens and navigation/Auth/Phone enter screen/PhoneEnterScreenController.swift
@@ -6,11 +6,22 @@
 //
 
 import UIKit
+import InputMask
 
 class PhoneEnterScreenController: BaseViewController {
   // MARK: - Properties
+  @IBOutlet private var phoneCodeLabel: UILabel!
+  @IBOutlet private var phoneField: UITextField!
+  @IBOutlet private var phoneFieldListener: MaskedTextFieldDelegate!
+  @IBOutlet private var activityIndicator: UIActivityIndicatorView!
+
   private let viewModel = PhoneEnterScreenViewModel()
 
+  // MARK: - Lifecycle methods
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
   // MARK: - Overridden methods
   override func createViewModel() {
     commonTypeViewModel = viewModel
@@ -18,5 +29,26 @@ class PhoneEnterScreenController: BaseViewController {
 
   override func addBindings() {
     super.addBindings()
+
+    viewModel.phoneCode.bind { [weak self] (phoneCodeString) in
+      self?.phoneCodeLabel.text = phoneCodeString
+    }
+    viewModel.showActivity.bind { [weak self] (needToShowActivity) in
+      if needToShowActivity {
+        self?.activityIndicator.startAnimating()
+      } else {
+        self?.activityIndicator.stopAnimating()
+      }
+    }
+    
+  }
+}
+
+// MARK: - Masked text field delegate listener methods
+extension PhoneEnterScreenController: MaskedTextFieldDelegateListener {
+  func textField(_ textField: UITextField, didFillMandatoryCharacters complete: Bool, didExtractValue value: String) {
+    guard complete else { return }
+    viewModel.setEnteredPhone(value)
+    phoneField.resignFirstResponder()
   }
 }
--- WhiteLabel-iOS/Common/Screens and navigation/Auth/Phone enter screen/PhoneEnterScreenViewModel.swift
+++ WhiteLabel-iOS/Common/Screens and navigation/Auth/Phone enter screen/PhoneEnterScreenViewModel.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 class PhoneEnterScreenViewModel: BaseViewModel {
+  // MARK: - Properties
+  let phoneCode = Box(value: "")
+  let showActivity = Box(value: false)
 
+  private let phoneCodeString = "+7"
+
+  // MARK: - Lifecycle methods
+  override init() {
+    super.init()
+    phoneCode.value = phoneCodeString
+  }
+
+  // MARK: - Internal/public custom methods
+  func setEnteredPhone(_ enteredPhoneString: String) {
+    print("Need to send phone \(phoneCodeString + enteredPhoneString)")
+    showActivity.value = true
+  }
 }
