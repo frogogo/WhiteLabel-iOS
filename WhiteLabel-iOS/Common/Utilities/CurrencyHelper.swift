006e3fc77e650229605519c39159bf37f8f4557b
Oleg Mosyagin <o.lego.work@gmail.com>
2021-10-08 15:01:59 +0500

    Add price display formats

--- WhiteLabel-iOS/Common/Utilities/CurrencyHelper.swift
+++ WhiteLabel-iOS/Common/Utilities/CurrencyHelper.swift
@@ -7,11 +7,25 @@
 
 import Foundation
 
+enum PriceDisplayFormat {
+  /// In this case price will be represented in this order: currency symbol, sum. Use it for dollar or euro display
+  case symbolSum
+  /// In this case price will be represented in this order: sum, space, currency symbol. Use it for russian rubles display
+  case sumSpaceSymbol
+}
+
 class CurrencyHelper {
   static func readableSum(withAmount sum: Double) -> String {
     let currencySymbol = Configuration.currencySymbol
     let pricePrecision = Configuration.pricePrecision
-    let formattedPrice = String(format: "%.\(pricePrecision)f", sum)
-    return "\(formattedPrice) \(currencySymbol)"
+    let format = Configuration.priceFormat
+    let formattedSum = String(format: "%.\(pricePrecision)f", sum)
+
+    switch format {
+    case .sumSpaceSymbol:
+      return "\(formattedSum) \(currencySymbol)"
+    case .symbolSum:
+      return "\(currencySymbol)\(formattedSum)"
+    }
   }
 }
